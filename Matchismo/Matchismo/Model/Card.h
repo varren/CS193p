1523ec44d9e030868de828840487523cdb64d134
Varren <varren@gmail.com>
2013-10-05 04:24:37 -0700

    File Structure assn 0(Matchismo) Created Video2

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by mmh on 05/10/2013.
+//  Copyright (c) 2013 mmh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/PlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by mmh on 05/10/2013.
+//  Copyright (c) 2013 mmh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayingCard : NSObject
+
+@end
