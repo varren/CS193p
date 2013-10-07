0c7edf7c47f9072fe7e2bda7b8ff1acedf563412
Varren <varren@gmail.com>
2013-10-07 13:46:31 -0700

    Video 4. hw1 Matchismo done. Extra credit left

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -18,31 +18,31 @@
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UILabel *lastFlipResultLable;
-@property (nonatomic) BOOL needNewDeck;
+@property (nonatomic) BOOL needGameRestart;
 
 @end
 
 @implementation CardGameViewController
 
 -(CardMatchingGame *)game{
-    if(!_game || self.needNewDeck) {
+    if(!_game || self.needGameRestart) {
         _game = [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count
                                                   usingDeck:[[PlayingCardDeck alloc]init]];
-        self.needNewDeck = NO;
+        self.needGameRestart = NO;
     }
     return _game;
 }
 
 - (IBAction)changeMode:(id)sender {
+    // if 1-st option selected (default) will start 2-cards matching game. else 3-card
     [self.game setMode: self.modeControl.selectedSegmentIndex ? 3 : 2];
-    
 }
 
 -(void) setCardButtons:(NSArray *)cardButtons{
     _cardButtons = cardButtons;
     [self updateUI];
 }
-
+# define DEFAULT_IMG_INSERTS 8
 -(void)updateUI{
     
     // updating cards view
@@ -50,8 +50,14 @@
         Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
         [cardButton setTitle:card.contents forState:UIControlStateSelected];
         [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+
+        UIImage *img = card.isFaceUp ? [UIImage imageNamed: @"clear" ] : [UIImage imageNamed: @"cardback.jpg" ] ;
+        [cardButton setImage: img forState:UIControlStateNormal];
+        [cardButton setImageEdgeInsets:UIEdgeInsetsMake(DEFAULT_IMG_INSERTS, DEFAULT_IMG_INSERTS, DEFAULT_IMG_INSERTS, DEFAULT_IMG_INSERTS)];
+  
         cardButton.selected = card.isFaceUp;
         cardButton.enabled = !card.isUnplayable;
+
         cardButton.alpha = card.isUnplayable ? 0.3 : 1.0;
     }
     
@@ -63,7 +69,7 @@
 }
 
 - (IBAction)reDealCards:(id)sender {
-    self.needNewDeck = YES;
+    self.needGameRestart = YES;
     self.flipCount = 0;
     self.modeControl.userInteractionEnabled = YES;
     [self updateUI];
