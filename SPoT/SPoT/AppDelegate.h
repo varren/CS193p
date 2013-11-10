bca13c58f945612deedd78c30b4931d229152695
Varren <varren@gmail.com>
2013-11-09 17:42:36 -0800

    Video 9 Had some rest time. Starting HW4 now

---
+++ SPoT/SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by mmh on 05/11/2013.
+//  Copyright (c) 2013 mmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT/SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by mmh on 05/11/2013.
+//  Copyright (c) 2013 mmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
